59103f84c9d1fe2f704828fa9f0c159c6e853df8
Hoós Roland <shadeeygg@gmail.com>
2023-01-10 18:11:00 +0100

    Pog

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+interface State {
+  datum: Date;
+  dobas: number;
+}
+
+class App extends Component<{}, State>{
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      datum: new Date(),
+      dobas: 0
+    }
+
+    
+    setInterval(() => {
+      //Mit csináljon 
+      this.setState({
+        datum:new Date()
+      });
+    }, 1000)
+  }
+
+  kockadobas = () => {
+    const veletlen = Math.floor(Math.random()*6+1)
+    this.setState({
+      dobas:veletlen
+    })
+  }
+
+
+  render(){
+    return <div>
+      <p id='ido' style={{
+        fontStyle: 'italic',
+        color: 'deeppink'
+
+      }}>Az aktuális idő: { this.state.datum.toLocaleString() }</p>
+      <p className="kockadobas">A dobás eredménye: { this.state.dobas.toLocaleString() }</p>
+    <button onClick={this.kockadobas}>Dobás</button>
     </div>
-  );
+  }
 }
 
 export default App;
